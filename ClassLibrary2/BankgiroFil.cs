0699e3b1c7ff8173ffdd9b7595c16813ec4a56ce
AndersMartini <andersmartini2@gmail.com>
2014-10-03 17:54:42 +0200

    added missing files

---
+++ ClassLibrary2/BankgiroFil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankgiro
+{
+    public class BankgiroFil
+    {
+        private string file;
+        private string kommunikationsSätt = "IBGLK";
+        private string kundNummer = "";
+
+        //Lägg till en grupp betalningar i filen. dessa skapas med avsnitt constructorn. Se avsnitt.cs 
+        public void addPayGroup(avsnitt avsnt)
+        {
+            string strng = avsnt.format();
+            file += strng + Environment.NewLine;
+        }
+
+        public void savefile()
+        {
+            
+            var fileName = "BFEP." + kommunikationsSätt + ".KO" + kundNummer +".txt";
+            var dir = @"C:\Users\Karl\Downloads\" + fileName;
+
+            System.IO.File.WriteAllText(dir, file);
+
+
+        }
+
+
+    }
+}
